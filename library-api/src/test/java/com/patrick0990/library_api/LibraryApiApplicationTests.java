b373a10d7dbe59e84961c3d41c250118d7a72665
patrick-0990 <patrickwallenstein0990@gmail.com>
2026-01-18 17:30:31 -0300

    Initial Commit: Estrutura do Projeto Spring Boot

---
+++ library-api/src/test/java/com/patrick0990/library_api/LibraryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patrick0990.library_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
